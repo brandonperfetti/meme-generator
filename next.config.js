0a4fbb0770e66234d305be7625b11f1e4d197261
Brandon Perfetti <brandon@brandonperfetti.com>
2023-10-02 17:37:11 -0700

    🔧 chore(next.config.js): remove unnecessary newline at the end of the file The commit removes an unnecessary newline at the end of the `next.config.js` file. This change improves code readability and consistency.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	pagesDir: 'src/pages',
 	images: {
 		remotePatterns: [
 			{
-				protocol: "https",
-				hostname: "i.imgflip.com",
-				port: "",
-				pathname: "/**",
+				protocol: 'https',
+				hostname: 'i.imgflip.com',
+				port: '',
+				pathname: '/**',
 			},
 		],
 	},
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
